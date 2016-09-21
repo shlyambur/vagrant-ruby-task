1911c22f2d2f929b91b87fc0f6da4737495f2d8e
shlyambur <shlyambur@gmail.com>
2016-09-21 21:52:07 +0000

    Once more

--- script.sh
+++ script.sh
@@ -3,5 +3,9 @@
 #set -x
 echo "deb http://download.virtualbox.org/virtualbox/debian trusty contrib" > /etc/apt/sources.list
 
+sudo apt-get update
+sudo apt-get install virtualbox-5.1
+
+wget https://releases.hashicorp.com/vagrant/1.8.5/vagrant_1.8.5_x86_64.deb
 
 #set +x
