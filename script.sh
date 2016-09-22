0fcdfd25236af338c02ff399261e5efa452f7a74
shlyambur <shlyambur@gmail.com>
2016-09-22 13:10:10 +0000

    AAAA! silly if...else string comperinggit status!

--- script.sh
+++ script.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 set -x
-
-echo "deb http://download.virtualbox.org/virtualbox/debian trusty contrib" > /etc/apt/sources.list
+filetxt=/etc/apt/sources.list
+linetxt="deb http://download.virtualbox.org/virtualbox/debian trusty contrib"
+if [[ "$(cat $filetxt | grep "$linetxt")" != *"$linetxt" ]]
+then
+        echo "$linetxt" | sudo tee --append /etc/apt/sources.list #> /dev/null
+else
+        exit
+fi
+set +x
 
 sudo apt-get update
-sudo apt-get install virtualbox-5.1
+sudo apt-get -y install virtualbox-5.1 --force-yes
 
 if [ "$(sudo getconf LONG_BIT)" = 32 ];
 then
@@ -13,12 +20,16 @@ then
         sudo dpkg -i /tmp/vagrant_1.8.5_i686.deb
 else
         curl "https://releases.hashicorp.com/vagrant/1.8.5/vagrant_1.8.5_x86_64.deb" -o "/tmp/vagrant_1.8.5_x86_64.deb"
-        sudo dpkg -i /tmp/vagrant_1.8.5_x86_64.deb"
+        sudo dpkg -i /tmp/vagrant_1.8.5_x86_64.deb
 fi
+
+set +x
 
 vagrant box add ubuntu/trusty64
 mkdir ~/vagrant/vm/node01
 cd ~/vagrant/vm/node01
 vagrant init ubuntu/trusty64
+vagrant up
+vagrant ssh
 
 set +x
